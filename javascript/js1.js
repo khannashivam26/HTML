d0c004f436fdc60bc1c702d617af65519b93d1e2
khannashivam26 <shivamkhanna6721.10e@gmail.com>
2025-02-12 20:21:00 +0530

    Update js1.js

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,7 +1,11 @@
-let num=document.querySelector('button');
-num.addEventListener('click',addnum);
 
-function addnum(){
-    let name=prompt;
-    
-}
+ let num=document.querySelector('button');
+ num.addEventListener('click',addNum);
+
+ function addNum(){
+    let name=prompt('enter name of student');
+    num.textContent='Roll No 1'+ name;
+
+ }
+var hh=4555655541n;
+console.log(typeof hh);
