0c73fd2843dbefbef3afb0b08055be80ad1a83dc
khannashivam26 <shivamkhanna6721.10e@gmail.com>
2025-02-12 20:22:54 +0530

    Create var x=52,y=10.js

---
+++ javascript/var x=52,y=10.js
@@ -0,0 +1,25 @@
+var x=52,y=10;
+         x=x%y;
+
+//exponential operator
+var a=10,b=2;
+a=a**2;
+//strcit operator(compares data types strictly)
+var s=3==="3";
+
+switch(devices){
+    case "fan":
+        console.log("Fan is turned on");
+        break;
+     case "tv":
+        console.log("TV is turned on");
+        break;
+    case "charger":
+        console.log("Charger is turned on");
+            break;
+    case "tubelight":
+        console.log("Tubelight is turned on");
+                break;
+    default:
+        console.log("no action")
+}
