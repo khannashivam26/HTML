1e37bff7f706d9b1ad6f4ab645f46d056b863b3f
khannashivam26 <shivamkhanna6721.10e@gmail.com>
2025-02-13 18:46:36 +0530

    new file

---
+++ javascript/ReferenceData.js
@@ -0,0 +1,21 @@
+let animal={
+    name:'dog',
+    legs:4
+}
+//dot notation
+console.log(animal.name);
+//bracket notation
+console.log(animal["name"]);
+console.log(animal["legs"]);
+//another way for bracket notation
+let legprop="legs";
+console.log(animal[legprop]);
+
+//ARRAYS
+let books=["english","maths","sst"];
+console.log(books);
+console.log(books[1]); //index particular element as per there index
+  
+
+let number=[4,5,6,7,8,9,10];
+console.log(number.length);
