284fdfc0c244dde4899795c6dc1cf853f05f002b
khannashivam26 <shivamkhanna6721.10e@gmail.com>
2025-03-10 23:44:21 +0530

    Update Class2.js

--- javascript/Class2.js
+++ javascript/Class2.js
@@ -1,19 +1,28 @@
-class Product{
-    constructor(itemName){
-        this.name=itemName;
+class Product {
+    constructor(itemName) {
+        this.name = itemName;
     }
-    getName(){
-        return this.name + "is the profuct name";
-    }
-}
-//creating a subclass (extending a class or inheritance)
-class Label extends Product{
-    constructor(name){
-        super(itemName);//super main class ke constructor ko call krta hai 
-    }
-    getname(){
-        return this.name+"is the label"
+    getName() {
+        return this.name + " is the product name";
     }
 }
-let label=new Label();
-console.log(label.getname())
+
+// Creating a subclass (extending a class or inheritance)
+class Label extends Product {
+    constructor(name) {
+        super(name); // Pass name to the super class constructor
+    }
+    getName() {  // Override the method
+        return this.name + " is the label";
+    }
+
+    // Method to call Product class's getName() method explicitly
+    getProductName() {
+        return super.getName(); // Call the parent class's method
+    }
+}
+
+let label = new Label("Product A");
+
+console.log(label.getName());        // Calls the overridden method in Label class
+console.log(label.getProductName()); // Calls the method from the Product class
